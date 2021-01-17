915a32370db6c12693ea30814712a512390e8b0a
Jordan <jconheasman@gmail.com>
2021-01-16 17:19:27 -0800

    added new routes to index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const uploadRoutes = require("./routes/api/uploads");
 
+app.use("*", cors());
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.status(200).send( {sucess:"bingo bango bongo"});
-});
+// app.get('/api/health', (req, res) => {
+//   res.status(200).send( {sucess:"bingo bango bongo"});
+// });
+app.use(uploadRoutes);
+// Currently cannot get due to aws connection issue
 
 app.listen(8000);
